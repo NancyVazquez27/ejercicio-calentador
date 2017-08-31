4a6b2a7b29ddc5425931240e5314efe8a6bfb662
Nancy <31119437+NancyVazquez27@users.noreply.github.com>
2017-08-31 00:02:12 -0500

    Se agrego la documetacion necesaria

--- Calentador.java
+++ Calentador.java
@@ -1,4 +1,14 @@
-public class Calentador
+/**\
+ * Esta clase fue creada para simular un calentador , el cual tiene una variable de instancia
+ * temperatura la cual es inicializada en 15, de igual manera tiene una segunda varibale de instacia
+ * que seria el incremento que aumentara la temperatura cada vez que se desee incrementar
+ * la tercera varible de instancia seria el valor maximo y la cuarto seria el valor minimo que pudiera
+ * alcanzar el calentador.
+ * 
+ * @param tempMax (seria la temperatura max que se desea alcance y no rebase el calentador)
+ * @param tempMin ( seria la temperatura min que se desea alcance y no rebase el calentador)
+ * 
+ */public class Calentador
 {
     private int temperatura; // Variables de instancia
     private int incremento;
@@ -12,19 +22,28 @@ public class Calentador
         incremento=3;
         temperatura=15;
     }
-    
+    /**
+     * Este metodo tiene como finalidad calentar el objeto calentador, ala varibale temperatura le sumara el incremento
+     * que fue especificado siempre y cuando la temperatura sea menor ala temperatura max.
+     * No recibe niguna variable como parametro.
+     * No regresa ningun tipo
+     */
     public  void calentar() // Metodo
     {
         if(temperatura<max)
         {
-            temperatura=temperatura+incremento;
-            if(temperatura>max)
+            temperatura+=incremento;
+            if(temperatura> max)
             {
-                temperatura=temperatura-incremento;
+                temperatura= temperatura-incremento;
             }
         }
     }
-    
+    /**
+     * Este metodo fue creado con la finalidad de simular que el calentador enfria, la varible temperatura debe cumplir 
+     * la condicion de que sea mayor que la temperatura min para poder reducir la misma. De lo contrario
+     * no permitira que se le disminuya.
+     */
     public void enfriar() // Constructor
     {
         if(temperatura>min)
@@ -37,20 +56,29 @@ public class Calentador
         }
         
     }
-    
+    /**
+     * Este metodo de acceso permite conocer la temperatura del calentador.
+     * No recibe parametros
+     * @return La temperatura del calentador.
+     */
     public int mostrarTemperatura() // Acceso
     {
         return(temperatura);
     }
-    
+    /**
+     * Este metodo de modificacion permite que el usuario especifique cual desea que sea el incremento del calentador
+     * @param recibe como parametro la variable increMod el cual debe tener el un valor mayor 0, de lo contrario no
+     * podra modificarse.
+     * No retorna nada.
+     */
     public void modificarIncremento(int increMod) // Constructor
     {
         if(increMod<0)
         {
-         incremento=0;
+            incremento=0;
         }
         else
-        {
+           {
             incremento=increMod;
         }
     }
